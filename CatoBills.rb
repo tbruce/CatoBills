db42c838b5d40d4a406f237708201076f2b6c079
tbruce <tom@liicornell.org>
2014-07-19 12:22:59 -0400

    First commit

---
+++ CatoBills.rb
@@ -0,0 +1,41 @@
+require 'json'
+require 'nokogiri'
+require 'net/http'
+require 'trollop'
+require 'rdf'
+INCLUDE RDF
+
+
+# Factory class for Cato bills
+def CatoBillFactory
+  def initialize
+
+  end
+  def fetch_bill_list
+
+  end
+  def triplify_bills
+
+  end
+  def jsonify_for_usc
+
+  end
+end
+
+def CatoBill
+  def initialize
+
+  end
+
+  # pull bill via Cato API
+  def fetch_bill
+
+  end
+  def extract_meta
+
+  end
+  def triplify
+
+  end
+
+end
