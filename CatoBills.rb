85320a2361ee29267d4f55992fddb5455065ada8
tbruce <tom@liicornell.org>
2014-07-20 08:55:21 -0400

    first draft of census feature

--- CatoBills.rb
+++ CatoBills.rb
@@ -8,35 +8,48 @@ include RDF
 BILL_LIST_URL = 'http://deepbills.cato.org/api/1/bills'
 BILL_API_PREFIX = 'http://deepbills.cato.org/api/1/bill?'
 
+# note on URI design for bills
+# generally, looks like http://liicornell.org/id/congress/bills/[congressnum]/[billtype]/[number]
+# where billtypes follow the GPO convention of h, s, hres, sres, hconres, sconres, hjres, sjres
+
+BILL_URI_PREFIX = 'http://liicornell.org/id/us/congress/bills'
+
 # Factory class for Cato bills
 class CatoBillFactory
   def initialize
     @bills = Hash.new()
     @cong_num = calc_Congress()
     fetch_bill_list
-    puts "checkpoint"
   end
 
   # get the Cato bill list and rubify
   def fetch_bill_list
     begin
-      bill_uri = URI(BILL_LIST_URL)
-      bill_json = Net::HTTP.get(bill_uri)
-      unless bill_json
+      bill_list_uri = URI(BILL_LIST_URL)
+      bill_list_json = Net::HTTP.get(bill_list_uri)
+      unless bill_list_json
         raise "Cato bill list unavailable from Cato server"
       end
     rescue Exception => e
       $stderr.puts e.message
       $stderr.puts e.backtrace.inspect
     end
-    @bills = JSON.parse(bill_json)
+    @bills = JSON.parse(bill_list_json)
   end
 
   def take_status_census
     census = Hash.new()
     @bills.each do |billparms|
       bill = CatoBill.new(billparms)
-
+      if census[bill.stage].nil?
+        census[bill.stage] = 1
+      else
+        census[bill.stage] = census[bill.stage] + 1
+      end
+    end
+    # print census sorted by values
+    census.sort_by {|btype, count| btype}.each do | stage, num |
+      puts "#{stage} : #{num}\n"
     end
   end
   def triplify_bills
@@ -56,46 +69,61 @@ class CatoBillFactory
 end
 
 class CatoBill
-  attr_reader :status
+
+  attr_reader :stage, :title, :legisnum, :type, :genre, :congress, :version, :uri, :pathish_uri
+
   def initialize (in_bill)
-    @bill = in_bill
-    @btype = nil
-    @status = nil
+
+    @type = in_bill['billtype']
+    @catonum = in_bill['billnumber']
+    @version = in_bill['billversion']
+    @congress = in_bill['congress']
+    @uri = nil
+    @pathish_uri = nil
+    @genre = nil
+    @stage = nil
+    @title = nil
+    @legisnum = nil
     @xml = fetch_bill
     extract_meta
   end
 
   # pull bill via Cato API
   def fetch_bill
-    params = "billnumber=#{@bill['billnumber']}&billversion=#{@bill['billversion']}&congress=#{@bill['congress']}&billtype=#{@bill['billtype']}"
+    params = "billnumber=#{@catonum}&billversion=#{@version}&congress=#{@congress}&billtype=#{@type}"
     begin
       bill_uri = URI(BILL_API_PREFIX + params)
-      billjson = Net::HTTP.get(bill_uri)
-      unless billjson
-        raise "Cato bill fetch failed for bill number #{bill['billnumber']}"
+      bill_json = Net::HTTP.get(bill_uri)
+      unless bill_json
+        raise "Cato bill fetch failed for bill number #{@catonum}"
       end
     rescue Exception => e
       $stderr.puts e.message
       $stderr.puts e.backtrace.inspect
     end
-    billhash = JSON.parse(billjson)
+    billhash = JSON.parse(bill_json)
     return billhash['billbody']
   end
+
+  # extract any interesting bill metadata
   def extract_meta
     @doc = Nokogiri::XML(@xml)
-    if @bill['billtype'] =~ /res$/
-      @btype = 'resolution'
+    if @type =~ /res$/
+      @genre = 'resolution'
       stageattr = 'resolution-stage'
     end
-    if @bill['billtype'] =~ /^(hr|s)$/
-      @btype = 'bill'
+    if @type =~ /^(hr|s)$/
+      @genre = 'bill'
       stageattr = 'bill-type'
     end
+    @stage = @doc.xpath("//#{@genre}").attr(stageattr).content
+    @title = @doc.xpath('//official-title').first.content
+    @legisnum = @doc.xpath('//legis-num').first.content
+    bflat = @legisnum.gsub(/\.*\s+/, '_').downcase
+    bnumber = @legisnum.split(/\s+/).last
 
-
-    @status = @doc.at_xpath("//#{@btype}[@='#{stageattr}']")["content"]
-
-    puts "woof"
+    @pathish_uri = "#{BILL_URI_PREFIX}/#{@congress}/#{@type}/#{bnumber}"
+    @uri = "#{BILL_URI_PREFIX}/#{@congress}_#{@bflat}"
 
   end
   def triplify
@@ -106,3 +134,4 @@ end
 
 f = CatoBillFactory.new
 f.take_status_census
+puts "done"
